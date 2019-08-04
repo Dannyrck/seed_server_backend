b537728daf21bd1243a8e4ade5adce84a8dc587b
Oscar Daniel Perez Espinoza <oskarcore@gmail.com>
2019-08-04 17:10:27 -0500

    new: se implementa funcionalidad para registar un usuario

--- controllers/resgistration_controller.js
+++ controllers/resgistration_controller.js
@@ -1,5 +1,18 @@
+const User = require('../models').User;
 module.exports = {
     new: (req, res)=>{
         res.render('registration/new');
+    },
+    /** Crea el alta de un nuevo usuario
+    */
+    postUsuario:(req,res)=>{
+        User.create({
+            email: req.body.email,
+            password: req.body.password
+        }).then((result)=> res.json(result))
+        .catch(err=> {
+            console.log(err);
+            res.json(err);
+        });
     }
 }
--- routes/registration_routes.js
+++ routes/registration_routes.js
@@ -4,5 +4,6 @@ const registrationController = require('../controllers/resgistration_controller'
 let router = express.Router();
 
 router.get('/signup', registrationController.new);
+router.route('/user').post(registrationController.postUsuario)
 
 module.exports = router;
