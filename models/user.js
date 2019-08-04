d0d91ee008c1bf758350a7b8c57c95daf8d5316c
Oscar Daniel Perez Espinoza <oskarcore@gmail.com>
2019-08-04 17:09:54 -0500

    new: se agrega funcionalidad para hashear el password de un usuario registrado

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 'use strict';
+
+const bcrypt = require('bcrypt-nodejs');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     email: {
@@ -6,10 +9,25 @@ module.exports = (sequelize, DataTypes) => {
       unique: true,
       allowNull: false
     },
-    password_hash: DataTypes.STRING
-  }, {});
+    password_hash: DataTypes.STRING,
+    password: DataTypes.VIRTUAL
+  }, {
+    hooks: {
+      beforeCreate:(user,options)=>{
+        return new Promise((res,rej)=>{
+          let salt = bcrypt.genSalt(10,(err,result)=>result);
+          bcrypt.hash(user.password,salt,null,(err,hash)=>{
+            user.password_hash = hash;
+          });
+          res();
+        });
+      }
+    }
+  });
   User.associate = function(models) {
     // associations can be defined here
   };
+ 
+
   return User;
 };
